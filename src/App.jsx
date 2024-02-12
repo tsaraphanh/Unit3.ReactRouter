8b66ab06a80eb970d3c50d46838e79c6ee117df8
Troy Saraphanh <troysaraphanh@Troys-MacBook-Air.local>
2024-02-12 12:54:00 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
+  )
+}
+
+export default App
