7b9c1d7d396190cab365c0e6cabfecac1621a3a0
Troy Saraphanh <troysaraphanh@Troys-MacBook-Air.local>
2024-02-12 13:37:00 -0600

    add app template & add red & blue component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
 
   return (
     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="navbar">
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/components/Blue.jsx
@@ -0,0 +1,7 @@
+export default function Blue() {
+    return (
+        <div className="Blue">
+            <h1>Blue</h1>
+        </div>
+    )
+}
